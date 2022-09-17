b7ab2e25281d4e58547cbb35f949b5fb9a5276ab
string_main <yhj2756@naver.com>
2022-09-17 20:37:07 +0900

    1-4: add type

--- udemy/first-project/js-only.js
+++ udemy/first-project/js-only.js
@@ -1,11 +1,12 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1");
-const input2 = document.getElementById("num2");
+// 같은 상수가 여러 파일에서 사용되면 에러 발생
+// const button = document.querySelector("button");
+// const input1 = document.getElementById("num1");
+// const input2 = document.getElementById("num2");
 
-function add(num1, num2) {
-  return num1 + num2;
-}
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
 
-button.addEventListener("click", function() {
-  console.log(add(input1.value, input2.value));
-});
+// button.addEventListener("click", function() {
+//   console.log(add(input1.value, input2.value));
+// });
