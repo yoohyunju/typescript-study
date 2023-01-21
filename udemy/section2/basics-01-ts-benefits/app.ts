f09e39dccb32f51b3ab53f770eab620d60971e56
string_main <yhj2756@naver.com>
2023-01-21 23:35:34 +0900

    2-19: 객체/배열/튜플 타입

--- udemy/section2/basics-01-ts-benefits/app.ts
+++ udemy/section2/basics-01-ts-benefits/app.ts
@@ -1,9 +1,30 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+// 명시적인 타입 지정은 좋은 방식이 아니다.
+// const person: {
+//   name: string;
+//   age: number;
+// } 
+
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  name: "Maximilian",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
+};
+
+person.role.push('admin');
+// person.role[1] = 10;
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
+console.log(person.name);
+
+for(const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase()); // 문자열 타입을 추론하여 인식
+  // console.log(hobby.map()); // 문자열 타입에는 map을 사용할 수 없어서 에러 발생
 }
-
-const number1 = 5;
-const number2 = 2.8;
-
-const result = add(number1, number2);
-console.log(result);
