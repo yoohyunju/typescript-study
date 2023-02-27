553a13f7ff39991074f69f9759fac8caa910ec6b
string_main <yhj2756@naver.com>
2023-02-27 23:58:18 +0900

    4-51: 기본값 함수 매개변수

---
+++ udemy/section4/next-gen-01-starting-setup/src/app.ts
@@ -0,0 +1,12 @@
+// 기본값 인수cd
+const add = (a: number, b: number = 1) => a + b;
+
+const printOutput : (a: number | string) => void = output => console.log(output);
+
+const button = document.querySelector('button');
+
+if(button) {
+  button.addEventListener('click', event => console.log(event));
+}
+
+printOutput(add(5));
